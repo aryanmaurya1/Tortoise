bf749443cdf06a0228a8ce29f0752964d139eb97
aryan <aryanmaurya1@outlook.com>
2020-06-28 21:36:51 +0530

    added random filling function.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var t Table
@@ -8,17 +10,18 @@ func main() {
 	t.Header = []string{"A", "B", "C"}
 	t.Rows = append(t.Rows, []float64{1, 2, 3})
 	t.Rows = append(t.Rows, []float64{3, 4, 5})
+	t.Rows = append(t.Rows, []float64{6, 7, 8})
+	t.Rows = append(t.Rows, []float64{17, 72, 78})
 	t.Rows = append(t.Rows, []float64{1, 2, 3})
 	t.Rows = append(t.Rows, []float64{1, 2, 3})
 	t.Rows = append(t.Rows, []float64{1, 2, 3})
-	t.Rows = append(t.Rows, []float64{1, 2, 3})
-	t.Rows = append(t.Rows, []float64{1, 2, 3})
-	t.Rows = append(t.Rows, []float64{1, 2, 3})
-	t.Rows = append(t.Rows, []float64{1, 2, 3})
+	t.Rows = append(t.Rows, []float64{100, 200, 300})
+	t.Rows = append(t.Rows, []float64{0, 0, 0})
 	// fmt.Println(EuclideanDistance(t.Rows[0], t.Rows[1]))
 
-	var model KNNModel
+	var model KMeansModel
 	model.Data = t
+	model.init(1)
 	fmt.Println(model)
 	// fmt.Println(t)
 }
--- models.go
+++ models.go
@@ -1,21 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-// KNNModel : Implements K Nearest Neighbhour clustering algorithm
-type KNNModel struct {
-	Name            string
-	Data            Table
-	Iterations      int // Number of iterations to run
-	NumCentroids    int
-	RandomCentroids bool
-	Centroids       []float64
-	Clusters        [][]float64
+// Point : contains distance from centroid and index of row
+type Point struct {
+	distance float64
+	rowIndex int
+}
+
+// KMeansModel : Implements K Nearest Neighbhour clustering algorithm
+type KMeansModel struct {
+	Name         string
+	Data         Table
+	Iterations   int // Number of iterations to run
+	NumCentroids int
+	Centroids    [][]float64
+	Clusters     [][]Point
 	/*
 		Row indexes of table which belongs to a particular cluster.
-		Ex :   [[i1, i2, i3], Indexs for cluster 1
-				[i1, i2, i3], Indexs for cluster 2
-				[i1, i2, i3], Indexs for cluster 3
+		Ex :   [[{distance, rowIndex}, {distance, rowIndex}, {distance, rowIndex}], Points for cluster 0
+				[i1, i2, i3], Points for cluster 1
+				[i1, i2, i3], Points for cluster 2
 				.
 				.
 				.
@@ -23,12 +32,49 @@ type KNNModel struct {
 	*/
 }
 
+func modifiedRow(n float64, row []float64) []float64 {
+	// modifies randomly choosen row, using seed n.
+	arr := make([]float64, len(row))
+	for i := 0; i < len(row); i++ {
+		arr[i] = row[i] * 0.5 * n
+	}
+	return arr
+}
+
+func fillCentroids(data *[][]float64, centroids *[][]float64) {
+	// fill centroids randomly
+	if len(*data) < len(*centroids) {
+		*centroids = nil
+	}
+	rand.Seed(time.Now().Unix())
+	var table = make([]int, len(*data), len(*data))
+	for i := 0; i < len(*centroids); i++ {
+	label:
+		randIndex := rand.Int() % len(*data)
+		if table[randIndex] == 1 {
+			goto label
+		}
+		table[randIndex] = 1
+		(*centroids)[i] = modifiedRow(2, ((*data)[randIndex]))
+	}
+}
+
+func (m *KMeansModel) init(nCentroids int) {
+	m.NumCentroids = nCentroids
+	m.Centroids = make([][]float64, nCentroids)
+	m.Clusters = make([][]Point, nCentroids)
+	fillCentroids(&m.Data.Rows, &m.Centroids)
+}
+
+func (m *KMeansModel) run(iterations int) {
+
+}
+
 // Used for printing the model
-func (m KNNModel) String() string {
+func (m KMeansModel) String() string {
 	fmt.Printf("%20s : %20v \n", "Model Name", m.Name)
 	fmt.Printf("%20s : %20v \n", "Iterations", m.Iterations)
 	fmt.Printf("%20s : %20v \n", "Number of Centroids", m.NumCentroids)
-	fmt.Printf("%20s : %20v \n", "Random Centroids", m.RandomCentroids)
 	fmt.Printf("\n %20s \n", "Attached Table")
 	fmt.Println(m.Data)
 	fmt.Println(fmt.Sprintf("%20s", "Centroids : "), m.Centroids)
